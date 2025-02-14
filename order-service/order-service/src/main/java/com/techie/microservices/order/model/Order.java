5b072faf92f5ea815e87f58cdfd4067f952c2674
agapehhhhhhh <114454472+agapehhhhhhh@users.noreply.github.com>
2025-02-14 17:49:04 +0700

    Initial commit

---
+++ order-service/order-service/src/main/java/com/techie/microservices/order/model/Order.java
@@ -0,0 +1,32 @@
+package com.techie.microservices.order.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
